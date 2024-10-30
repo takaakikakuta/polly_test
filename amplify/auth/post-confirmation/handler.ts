75f3461cd5baead938f087df783c31430ee21ea6
takaaki <soci0926@gmail.com>
2024-10-31 08:22:39 +0900

    add signup tenantid 3

--- amplify/auth/post-confirmation/handler.ts
+++ amplify/auth/post-confirmation/handler.ts
@@ -10,16 +10,10 @@ const client = new CognitoIdentityProviderClient();
 
 // add user to group
 export const handler: PostConfirmationTriggerHandler = async (event) => {
-  const command = new AdminUpdateUserAttributesCommand({
-    // GroupName: env.GROUP_NAME,
+  const command = new AdminAddUserToGroupCommand({
+    GroupName: env.GROUP_NAME,
     Username: event.userName,
     UserPoolId: event.userPoolId,
-    UserAttributes: [
-      {
-        Name: 'custom:tenant_id', // 更新するカスタム属性の名前
-        Value: event.userName     // カスタム属性にコピーするID
-      },
-    ],
   });
   const response = await client.send(command);
   console.log('processed', response.$metadata.requestId);
